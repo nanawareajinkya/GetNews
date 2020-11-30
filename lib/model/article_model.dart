0afe9374e0fa3e392e2baa56d1f4f8c785075760
Ajinkya <42488697+nanawareajinkya@users.noreply.github.com>
2020-11-30 20:43:35 +0530

    Create article_model.dart

---
+++ lib/model/article_model.dart
@@ -0,0 +1,11 @@
+class ArticleModel{
+
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String content;
+
+  ArticleModel({this.title,this.author,this.description,this.url,this.urlToImage,this.content});
+}
